e7037e34b45ae0a40b4221d1f84819ccd7dac26c
David Jones <maroon127@gmail.com>
2016-12-10 22:14:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,35 @@
-
 #include <iostream>
 #include "matrix.h"
 
+/* Test File */
+
 int main() {
-  int arr33[9] = { 1, 4, 2,
-		   1, 5, 6,
-		   2, 1, 8 };
+  double arr33[9] = { 1, 2, 3,
+		                  0, 4, 5,
+		                  1, 0, 6 };
 
-  int arr13[3] = {1,
-		  3,
-		  8 };
+  Matrix Z(3,3, arr33);
 
-  int arr31[3] = { 4, 2, 1 };
+  std::cout << "Matrix A:" << std::endl;
+  Z.printMatrix();
+  
+  std::cout << "Matrix A * A" << std::endl;
+  Z.multiply(Z).printMatrix();
+  
+  std::cout << "Cofactor Matrix: " << std::endl;
+  Z.cofactor().printMatrix();
+  
+  std::cout << "Adjoint (C^T)" << std::endl;
+  Z.cofactor().transpose().printMatrix();
+  
+  std::cout << "det(A):" << Z.determinant() << std::endl;
+  
+  std::cout << "\nInverse of A: " << std::endl;
+  Z.inverse().printMatrix();
+  
+  std::cout << "Identity Matrix from these Matrices: " << std::endl;
+  Z.multiply(Z.inverse()).printMatrix();
 
-  Matrix<9> X(3,3, arr33);
-  X.printMatrix();
-
-  //  Matrix<9> Y;
-  //Y= X;
-  //Y.printMatrix();
-
-  // Matrix<int> Z = X;
-  // Z = Y.add(X);
-  // Z.printMatrix();
-
+  
   return 0;
 }
